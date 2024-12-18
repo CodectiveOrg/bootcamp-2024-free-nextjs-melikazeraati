750646fe12491d36b571914c731a33c541c4ebcb
melika_zeraati <91113873+melikazeraati@users.noreply.github.com>
2024-12-18 23:12:53 +0330

    Lesson 1 (#2)
    * init: initial project
    
    * chore: remove font
    
    * chore: remove unnecessary codes
    
    * chore: add init title
    
    * chore: add vazirmatn font
    
    ---------
    
    Co-authored-by: milad014 <m.geravand@asax.ir>

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from 'next/font/google'
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "رزرو",
+  description: "سامانه جامع رزرو آنلاین",
+};
+
+const vazirmatn = Vazirmatn({
+  subsets: ['latin', 'arabic'],
+  display: 'swap',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body className={vazirmatn.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
